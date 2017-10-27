040b929d37ba12c0548839fe19e40ee590a0701f
Dennis Schenk <dennis@gridonic.ch>
2017-10-27 16:36:37 +0200

    Add simple test (currently failing)

--- index.test.js
+++ index.test.js
@@ -1,3 +1,19 @@
-test('test', () => {
-  expect(true).toBe(true);
+var postcss = require('postcss');
+var plugin = require('./');
+
+function run(input, output, opts) {
+
+    return postcss([ plugin(opts) ]).process(input)
+        .then(result => {
+            expect(result.css).toEqual(output);
+            expect(result.warnings().length).toBe(0);
+        });
+}
+
+it('should replace version string in a comment', () => {
+    return run(
+        '/* CustomCSS Library v{{ version }} */', 
+        '/* CustomCSS Library v1.3.7 */', 
+        { data: { version: '1.3.7' }}
+    );
 });
