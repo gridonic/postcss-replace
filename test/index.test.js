e40d63442d1faec35a45f0c7a8278b790df0ec64
Dennis Schenk <dennis@gridonic.ch>
2017-10-28 14:04:34 +0200

    Add more initial tests. Add basic travis config file.

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,16 @@
-var postcss = require('postcss');
-var plugin = require('../index');
+const postcss = require('postcss');
+const plugin = require('../index');
 
+/**
+ * Runs the plugins process function. Tests wether the given input is equal
+ * to the given expected output with the given options
+ *
+ * @param {string} input valid CSS string.
+ * @param {string} output valid CSS string.
+ * @param {object} opts options to be used by the plugin.
+ * @return {Function | any}
+ */
 function run(input, output, opts) {
-
     return postcss([ plugin(opts) ]).process(input)
         .then(result => {
             expect(result.css).toEqual(output);
@@ -10,10 +18,42 @@ function run(input, output, opts) {
         });
 }
 
-it('should replace version string in a comment', () => {
+it('Should replace version string in a comment', () => {
     return run(
-        '/* CustomCSS Library v{{ version }} */',
+        '/* CustomCSS Library v{{version}} */',
         '/* CustomCSS Library v1.3.7 */',
         { data: { version: '1.3.7' }}
+    );
+});
+
+it('Should replaces keywords without spaces', () => {
+    return run(
+        '/* CustomCSS Library v{{version}} */',
+        '/* CustomCSS Library v1.3.7 */',
+        { data: { version: '1.3.7' }}
+    );
+});
+
+it('Should replaces keywords without space at the end', () => {
+    return run(
+        '/* CustomCSS Library v{{version }} */',
+        '/* CustomCSS Library v1.3.7 */',
+        { data: { version: '1.3.7' }}
+    );
+});
+
+it('Should replaces keywords without space at the beginning', () => {
+    return run(
+        '/* CustomCSS Library v{{ version}} */',
+        '/* CustomCSS Library v1.3.7 */',
+        { data: { version: '1.3.7' }}
+    );
+});
+
+it('Should not replace anything in a non-comment when commentOnly option is given', () => {
+    return run(
+        'a { content: "{{ value }}"; }',
+        'a { content: "{{ value }}"; }',
+        { data: { value: 'test' }, commentsOnly: true }
     );
 });
