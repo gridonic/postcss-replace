e46d37f165dc0813fb105c343ab78f33cad036ec
Sebastian Prein <basti@gridonic.ch>
2017-10-28 14:55:14 +0200

    Use fixtures for testing

--- test/index.test.js
+++ test/index.test.js
@@ -1,59 +1,31 @@
 const postcss = require('postcss');
 const plugin = require('../index');
+const pkg = require('../package.json');
+const fs = require('fs');
 
 /**
- * Runs the plugins process function. Tests wether the given input is equal
- * to the given expected output with the given options
+ * Runs the plugins process function. Tests whether the given input is equal
+ * to the expected output with the given options.
  *
- * @param {string} input valid CSS string.
- * @param {string} output valid CSS string.
- * @param {object} opts options to be used by the plugin.
- * @return {Function | any}
+ * @param {string} input Input fixture file.
+ * @param {object} opts Options to be used by the plugin.
+ * @return {function}
  */
-function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
+function run(input, opts) {
+    const raw = fs.readFileSync(`./test/fixtures/${input}.css`, 'utf8');
+    const expected = fs.readFileSync(`./test/fixtures/${input}.expected.css`, 'utf8');
+
+    return postcss([plugin(opts)]).process(raw)
         .then(result => {
-            expect(result.css).toEqual(output);
+            expect(result.css).toEqual(expected);
             expect(result.warnings().length).toBe(0);
         });
 }
 
-it('Should replace version string in a comment', () => {
-    return run(
-        '/* CustomCSS Library v{{version}} */',
-        '/* CustomCSS Library v1.3.7 */',
-        { data: { version: '1.3.7' }}
-    );
+it('Should replace “name” string in a comment/value.', () => {
+    return run('basic', { data: pkg });
 });
 
-it('Should replaces keywords without spaces', () => {
-    return run(
-        '/* CustomCSS Library v{{version}} */',
-        '/* CustomCSS Library v1.3.7 */',
-        { data: { version: '1.3.7' }}
-    );
-});
-
-it('Should replaces keywords without space at the end', () => {
-    return run(
-        '/* CustomCSS Library v{{version }} */',
-        '/* CustomCSS Library v1.3.7 */',
-        { data: { version: '1.3.7' }}
-    );
-});
-
-it('Should replaces keywords without space at the beginning', () => {
-    return run(
-        '/* CustomCSS Library v{{ version}} */',
-        '/* CustomCSS Library v1.3.7 */',
-        { data: { version: '1.3.7' }}
-    );
-});
-
-it('Should not replace anything in a non-comment when commentOnly option is given', () => {
-    return run(
-        'a { content: "{{ value }}"; }',
-        'a { content: "{{ value }}"; }',
-        { data: { value: 'test' }, commentsOnly: true }
-    );
+it('Should NOT replace anything in a value, when “commentsOnly” option is set to TRUE.', () => {
+    return run('commentsOnly', { data: pkg, commentsOnly: true });
 });
